fddb1805f9662f38b6a75828a71f48a2b61fbcc4
Ryut4919 <tzhw.lai@gmail.com>
2018-12-07 18:39:18 +0900

    Origin
    0%
    charactor Move->Ok
    Camera follow->Ok

---
+++ TechC_RunForYourLife/Assets/MainScene/Script/Character/CharacterControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+    public float MoveSpeed;
+    private Vector3 Direction;
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+        _animator = this.GetComponent <Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var xDir = Input.GetAxis("Horizontal") * Time.deltaTime;
+        var zDir = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        Direction = new Vector3(xDir, 0, zDir);
+
+        if (Direction.sqrMagnitude > 0f)
+        {
+            Vector3 des = transform.position + Direction.normalized * MoveSpeed;
+            Quaternion Roll = Quaternion.LookRotation(des - transform.position, Vector3.up);
+
+            transform.position = des;
+            transform.rotation = Roll;
+
+            _animator.SetBool("Run", true);
+        }
+        else
+        {
+            _animator.SetBool("Run", false);
+        }
+        	
+	}
+    void OnCallChangeFace()
+    {
+
+    }
+}
